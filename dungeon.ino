16f7ccb96ec47a8eabd26d9761faa6b7f69d60f3
wuuff <wuuuufff@gmail.com>
2017-08-13 16:56:10 -0500

    Started refactoring, temporarily disabled battles
    I'm moving the different parts of the game into separate files to
    better manage it.  For now the entire battle system is disabled.

---
+++ dungeon.ino
@@ -0,0 +1,59 @@
+#ifdef NOTDEFINED
+byte map_cache[16][16];
+
+byte cache_originx = 0;
+byte cache_originy = 0;
+
+void draw_cache(){
+  for(byte i = 0; i < 16; i++){
+    for(byte j = 0; j < 16; j++){
+      if( map_cache[i][j] == WATER ){
+        if( worldframe == 0 || worldframe == 1 ){
+          gb.display.drawBitmap((cache_originx-dudex)+(j*8)+(SCREEN_WIDTH/2-4),(cache_originy-dudey)+(i*8)+(SCREEN_HEIGHT/2-4),water1);
+        }else{
+          gb.display.drawBitmap((cache_originx-dudex)+(j*8)+(SCREEN_WIDTH/2-4),(cache_originy-dudey)+(i*8)+(SCREEN_HEIGHT/2-4),water2);
+        }
+      }else if( map_cache[i][j] == DRAIN ){
+        if( worldframe == 0 || worldframe == 1 ){
+          gb.display.drawBitmap((cache_originx-dudex)+(j*8)+(SCREEN_WIDTH/2-4),(cache_originy-dudey)+(i*8)+(SCREEN_HEIGHT/2-4),drain1);
+        }else{
+          gb.display.drawBitmap((cache_originx-dudex)+(j*8)+(SCREEN_WIDTH/2-4),(cache_originy-dudey)+(i*8)+(SCREEN_HEIGHT/2-4),drain2);
+        }
+      }else if( map_cache[i][j] == FLOW ){
+        if( worldframe == 0 || worldframe == 1 ){
+          gb.display.drawBitmap((cache_originx-dudex)+(j*8)+(SCREEN_WIDTH/2-4),(cache_originy-dudey)+(i*8)+(SCREEN_HEIGHT/2-4),flow1);
+        }else{
+          gb.display.drawBitmap((cache_originx-dudex)+(j*8)+(SCREEN_WIDTH/2-4),(cache_originy-dudey)+(i*8)+(SCREEN_HEIGHT/2-4),flow2);
+        }
+      }else if( map_cache[i][j] != 0 ){
+        gb.display.drawBitmap((cache_originx-dudex)+(j*8)+(SCREEN_WIDTH/2-4),(cache_originy-dudey)+(i*8)+(SCREEN_HEIGHT/2-4),&tiles[(map_cache[i][j])*10]);
+      }
+    }
+  }
+  worldframe+=1;
+  worldframe%=4;
+}
+
+byte test_collision(byte dir){
+  return 0;
+
+
+
+
+  
+  if( dir == UP ){
+    gb.display.println(map_cache[(cache_originy+dudey-1)/8][(cache_originx+dudex)/8]);
+    return map_cache[(cache_originy+dudey-1)/8][(cache_originx+dudex)/8] != 0 ||
+      map_cache[(cache_originy+dudey-1)/8][(cache_originx+dudex+7)/8] != 0;
+  }else if( dir == DOWN ){
+    return map_cache[(cache_originy+dudey+8)/8][(cache_originx+dudex)/8] != 0 ||
+      map_cache[(cache_originy+dudey+8)/8][(cache_originx+dudex+7)/8] != 0;
+  }else if( dir == LEFT ){
+    return map_cache[(cache_originy+dudey)/8][(cache_originx+dudex-1)/8] != 0 ||
+      map_cache[(cache_originy+dudey+7)/8][(cache_originx+dudex-1)/8] != 0;
+  }else{
+    return map_cache[(cache_originy+dudey)/8][(cache_originx+dudex+8)/8] != 0 ||
+      map_cache[(cache_originy+dudey+7)/8][(cache_originx+dudex+8)/8] != 0;
+  }
+}
+#endif
---
+++ overworld.h
@@ -0,0 +1,24 @@
+#define BRICK 0
+#define COLUMN 1
+#define COLUMNCAP 2
+#define ROOF 3
+#define WINDOW 4
+#define STREETLIGHT 5
+#define POLE 6
+#define DOOR 7
+
+#define DRAIN 5
+#define FLOW 7
+#define WATER 17
+#define DOCK1 21
+#define DOCK2 23
+#define DOCK3 43
+
+#define UP 1
+#define DOWN 2
+#define LEFT 3
+#define RIGHT 4
+
+extern byte dudeframe;
+
+
