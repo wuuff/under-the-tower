b3ecd5acee78688ef1179699bc0993d445c03f3b
wuuff <wuuuufff@gmail.com>
2017-11-13 06:06:17 -0600

    Compressed dialogue with a Huffman code
    All the dialogue has now been replaced with a Huffman tree and the
    Huffman-encoded compressed text.  Since the Huffman tree I generate
    is not complete, I couldn't store it implicitly, but I wanted to
    avoid using pointers.  I knew I would have less than 255 nodes, so
    I encode the tree such that each "pointer" to a child node is
    actually just a 1-byte offset into an array of nodes.  Each internal
    node contains 255 for its data, and all leaf nodes have 0 for their
    children, although I never actually bother to read the latter. I
    figured that any more compact encoding might cost too much space
    in the code required for decoding it, since my priority is space
    optimization, not speed optimization.
    
    This compression saves only 302 bytes over the uncompressed
    version, although this also includes the cost of the decompression
    code. Also, all future text I add will contribute less to the size.
    I do think I can combine the dialogue_seek and fill_dialogue_buffer
    functions to save space, but I'm leaving them separate for the
    moment because I wrote them this way for clarity. I'll try to
    combine the two now that I have things working. Regardless, for now
    the new size is 29782 (97%) versus 30084 (98%) from before.
    
    Both the Huffman tree and compressed text are generated
    automatically by a separate script I wrote.  This means that any
    future text I add will automatically have an optimal code generated
    based on the new frequencies of the characters it contains.  If I
    add any new symbols, the tree will expand to accommodate them, so
    it's best if I limit how many unique characters are contained in
    the text.

--- dialogue.h
+++ dialogue.h
@@ -1,37 +1,37 @@
-extern uint8_t dialogue_index;
+extern uint16_t dialogue_index;
 extern uint8_t dialogue_remaining;
 
-extern void display_dialogue(uint8_t, uint8_t, uint8_t, const char[][8]);
+extern void display_dialogue(uint16_t, uint8_t, uint8_t, const char[][8]);
 
 #define TXT_INTRO 0
 #define TXT_INTRO_LEN 6
 
-#define TXT_SDW_INTRO 18
+#define TXT_SDW_INTRO 228
 #define TXT_SDW_INTRO_LEN 5
 
-#define TXT_SDW_CATPAW 35
+#define TXT_SDW_CATPAW 460
 #define TXT_SDW_CATPAW_LEN 0
 
-#define TXT_SLAVER 37
+#define TXT_SLAVER 483
 #define TXT_SLAVER_LEN 0
 
-#define TXT_GIRL_THX 40
+#define TXT_GIRL_THX 523
 #define TXT_GIRL_THX_LEN 2
 
-#define TXT_SDW_BATTLE 46
+#define TXT_SDW_BATTLE 603
 #define TXT_SDW_BATTLE_LEN 0
 
-#define TXT_SDW_WIN 49
+#define TXT_SDW_WIN 646
 #define TXT_SDW_WIN_LEN 0
 
-#define TXT_GIRL_FATHER 52
+#define TXT_GIRL_FATHER 685
 #define TXT_GIRL_FATHER_LEN 0
 
-#define TXT_ENEMY 55
+#define TXT_ENEMY 718
 #define TXT_ENEMY_LEN 0
 
-#define TXT_FATHER 56
+#define TXT_FATHER 734
 #define TXT_FATHER_LEN 3
 
-#define TXT_SDW_SHIP 68
+#define TXT_SDW_SHIP 892
 #define TXT_SDW_SHIP_LEN 2
